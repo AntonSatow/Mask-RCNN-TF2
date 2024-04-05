1944a5ca33d0c6ec4c446986b3fbdf9972ca56ce
AntonSatow <106592786+AntonSatow@users.noreply.github.com>
2024-04-05 20:02:58 +0200

    Modifying image with colour change

--- filter_test.py
+++ filter_test.py
@@ -5,68 +5,75 @@ import openpyxl
 from openpyxl import Workbook
 
 
-original_image = cv.imread("19_frame.jpg")
-resize_image = cv.resize(original_image, (1024, 768))
 
-#bilateral_blur = cv.bilateralFilter(resize_image, 9, 75, 75)
+def colour_change(image):
+    """
+    The function takes an image as input and returns a modified image
+    The function increases the intensity of the red,green,blue channels by factor
+    
+    Parameters:
+    image (numpy.ndarray): The image to modify
+    
+    Returns:
+    numpy.ndarray: The modified image
+    """
+    # Split the image into its individual color channels
+    r, g, b = cv.split(image)
 
-#grey_image = cv.cvtColor(resize_image, cv.COLOR_BGR2GRAY)
-hist = cv.calcHist([resize_image], [0], None, [256], [0, 256])
+    # Increase the intensity of the red channel by factor
+    r = cv.convertScaleAbs(r, alpha=0.5, beta=0)
 
-spike = np.argmax(hist)
-mask = resize_image == spike
-resize_image[mask] = [0, 0, 255]
+    # Increase the intensity of the green channel by factor
+    g = cv.convertScaleAbs(g, alpha=5, beta=0)
+    
+    # Decrease the intensity of the blue channel by factor
+    b = cv.convertScaleAbs(b, alpha=10, beta=0)
 
+    # Merge the color channels back together
+    modified_image = cv.merge((r, g, b))
+    return modified_image
+   
+
+def draw_coordinates(event, x, y, flags, param):
+    """
+    Display the coordinates of the mouse cursor on the image
+    Display the RGB values of the pixel at the cursor position
+    Display the image with the coordinates and pixel values on the top right corner
+    
+    Args:
+        event (_type_): The type of mouse event (e.g., cv.EVENT_MOUSEMOVE)
+        x (int): The current x-coordinate of the mouse cursor
+        y (int): The current y-coordinate of the mouse cursor
+        flags (int): Additional flags for the mouse event
+        param (image): The image to display the coordinates on
+    """
+    if event == cv.EVENT_MOUSEMOVE:
+        # Copy the original image so we don't draw on it permanently
+        img = param.copy()
+        
+        b, g, r = img[y, x]
+
+        # Draw the coordinates on the image
+        cv.putText(img, f"X: {x}, Y: {y}, R: {r}, G: {g}, B: {b}",(10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        # Display the image
+        cv.imshow("Original Image vs Gaussian Blur", img)
+
+original_image = cv.imread("test_img.png")
+original_image = cv.cvtColor(original_image, cv.COLOR_BGR2RGB)
+height = original_image.shape[0]
+bottom_half = original_image[height//2:,:]
+resize_image = cv.resize(bottom_half, (3000, 920))
+
+
+image = colour_change(resize_image)
+
+image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
+cv.imshow("Original Image vs Gaussian Blur", image)
+# Set the mouse callback function to the draw_coordinates function
+cv.setMouseCallback("Original Image vs Gaussian Blur", draw_coordinates, param=image)
 while True:
-    #side_by_side = np.hstack((resize_image, bilateral_blur))
-    cv.imshow("Original Image vs Gaussian Blur", resize_image)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 
-#image = original_image.copy()
-
-#original_image, image = cv.resize(original_image, (640, 480)), cv.resize(image, (640, 480))
-#grey_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-
-#lowpass filter
-#kernel = np.array([[1,1,1], [1,1,1], [1,1,1]]) * (1/50.0)
-#image = cv.filter2D(image, -1, kernel)
-
-# Create a lookup table
-#lut = np.zeros(256, dtype = image.dtype )# create empty array
-
-#for i in range(256):
-    #lut[i] = np.clip(pow(i / 255.0, 0.5) * 255.0, 0, 255)
-
-# Apply the lookup table
-#image = cv.LUT(image, lut)
-
-#Draw contours to images
-#image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-#ret, thresh = cv.threshold(image, 127, 255, cv.THRESH_BINARY)
-
-# Find contours
-#contours, hierarchy = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-#contour_image = original_image.copy()
-#cv.drawContours(contour_image, contours, -1, (0, 255, 0), 2)
-
-#histogram
-# create the histogram
-#hist,bins = np.histogram(image.flatten(),256,[0,256])
- 
-#cdf = hist.cumsum()
-#cdf_normalized = cdf * float(hist.max()) / cdf.max()
- 
-#plt.plot(cdf_normalized, color = 'b')
-#plt.hist(image.flatten(),256,[0,256], color = 'r')
-#plt.xlim([0,256])
-#plt.legend(('cdf','histogram'), loc = 'upper left')
-#plt.show()
-#cdf_m = np.ma.masked_equal(cdf,0)
-#cdf_m = (cdf_m - cdf_m.min())*255/(cdf_m.max()-cdf_m.min())
-#cdf = np.ma.filled(cdf_m,0).astype('uint8')
-#cv.equalizeHist(grey_image, image)
-# Stack the original and filtered images side by side
-#side_by_side = np.hstack((original_image, contour_image))
-#side_by_side = np.hstack((original_image, image))
